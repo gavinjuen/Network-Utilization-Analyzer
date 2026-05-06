4a5b7df513a91c0a9446dfaee7232a9e8fa8477d
Gavin Teo Juen <gavinteo1998@gmail.com>
2026-05-06 14:18:10 +0800

    add url

--- ring_portal/urls.py
+++ ring_portal/urls.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
-
+from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("dashboard.urls")),
-      path("", views.upload_view, name="upload"),
+    path("", views.upload_view, name="upload"),
     path("result/", views.result_view, name="result"),
     path("proof-data/", views.proof_data_view, name="proof_data"),
     path("download-excel/", views.download_excel_view, name="download_excel"),
