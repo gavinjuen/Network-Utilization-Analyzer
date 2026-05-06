b2ed6636a1b62023a8ed365563d6667ab463c666
Gavin Teo Juen <gavinteo1998@gmail.com>
2026-05-06 20:49:17 +0800

    update url

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     path("", views.upload_view, name="upload"),
     path("result/", views.result_view, name="result"),
-    path("result/proof-data/", views.proof_data_view, name="proof_data"),
-    path("download/excel/", views.download_excel_view, name="download_excel"),
+    path("proof-data/", views.proof_data_view, name="proof_data"),
+    path("download-excel/", views.download_excel_view, name="download_excel"),
 ]
--- ring_portal/wsgi.py
+++ ring_portal/wsgi.py
@@ -1,16 +1,15 @@
 import os
-from django.core.wsgi import get_wsgi_application
 import sys
 
 project_home = "/home/gavinjuen/Network-Utilization-Analyzer"
 if project_home not in sys.path:
     sys.path.insert(0, project_home)
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ring_portal.settings")
-application = get_wsgi_application()
-
 os.environ["SECRET_KEY"] = "your-secret-key-here"
 os.environ["DEBUG"] = "False"
 os.environ["ALLOWED_HOSTS"] = "gavinjuen.pythonanywhere.com"
 os.environ["CSRF_TRUSTED_ORIGINS"] = "https://gavinjuen.pythonanywhere.com"
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ring_portal.settings")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
