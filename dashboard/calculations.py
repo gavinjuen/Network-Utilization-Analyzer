4c98613e7019b0725bb6520fc800b7021b665cab
Gavin Teo Juen <gavinteo1998@gmail.com>
2026-05-19 08:10:47 +0800

    modify date

--- dashboard/calculations.py
+++ dashboard/calculations.py
@@ -217,9 +217,15 @@ def prepare_dataframe(combined):
     combined = combined.copy()
     combined["Collection Time"] = pd.to_datetime(
     combined["Collection Time"],
-    format="%Y-%m-%d %H:%M:%S",
     errors="coerce"
 )
+
+    if combined["Collection Time"].isna().all():
+        combined["Collection Time"] = pd.to_datetime(
+            combined["Collection Time"],
+            dayfirst=True,
+            errors="coerce"
+    )
     combined["TX_bps"] = combined["TXBPS(bit/s)"].apply(parse_bps)
     combined["RX_bps"] = combined["RXBPS(bit/s)"].apply(parse_bps)
     combined["MAX_bps"] = combined[["TX_bps", "RX_bps"]].max(axis=1)
